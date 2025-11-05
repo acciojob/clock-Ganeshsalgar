4ecade6711415c48c92790d91a98621d4821ee94
Ganesh Salgar <154046754+Ganeshsalgar@users.noreply.github.com>
2025-11-05 21:17:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function updataTimer(){
+	let now =new Date().toLocaleString();
+	document.getElementById("timer").textContent = now;
+}
+
+updataTimer();
+
+setInterval(updataTimer , 1000);
